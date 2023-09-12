bb66cad79271e3ba042af0f65c76919c7c371d84
ooples <cheatcountry@gmail.com>
2023-09-11 22:41:17 -0400

    Add project files.

---
+++ IRegression.cs
@@ -0,0 +1,12 @@
+﻿namespace AiDotNet;
+
+public interface IRegression
+{
+    double Predict(double[] x);
+
+    void Fit(double[][] x, double[] y);
+
+    double Score(double[][] x, double[] y);
+
+    double[] Normalize(double[] x);
+}
---
+++ SimpleRegression.cs
@@ -0,0 +1,24 @@
+﻿namespace AiDotNet;
+
+internal class SimpleRegression : IRegression
+{
+    public double Predict(double[] x)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Fit(double[][] x, double[] y)
+    {
+        throw new NotImplementedException();
+    }
+
+    public double Score(double[][] x, double[] y)
+    {
+        throw new NotImplementedException();
+    }
+
+    public double[] Normalize(double[] x)
+    {
+        throw new NotImplementedException();
+    }
+}
