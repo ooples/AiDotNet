daa8e1ff4d753baf9c2adebfc9e861582f80a133
Franklin Moormann <cheatcountry@gmail.com>
2025-11-15 17:03:17 -0500

    fix: add validation for nstepqlearningoptions properties
    - Validate StateSize > 0
    - Validate ActionSize > 0
    - Validate NSteps > 0
    - Throw ArgumentException with clear messages for invalid values
    - Fixes PR #481 review comment #65

--- src/Models/Options/NStepQLearningOptions.cs
+++ src/Models/Options/NStepQLearningOptions.cs
@@ -4,7 +4,46 @@ namespace AiDotNet.Models.Options;
 
 public class NStepQLearningOptions<T> : ReinforcementLearningOptions<T>
 {
-    public int StateSize { get; init; }
-    public int ActionSize { get; init; }
-    public int NSteps { get; init; } = 3;
+    private int _stateSize;
+    private int _actionSize;
+    private int _nSteps = 3;
+
+    public int StateSize
+    {
+        get => _stateSize;
+        init
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("StateSize must be greater than 0", nameof(StateSize));
+            }
+            _stateSize = value;
+        }
+    }
+
+    public int ActionSize
+    {
+        get => _actionSize;
+        init
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("ActionSize must be greater than 0", nameof(ActionSize));
+            }
+            _actionSize = value;
+        }
+    }
+
+    public int NSteps
+    {
+        get => _nSteps;
+        init
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("NSteps must be greater than 0", nameof(NSteps));
+            }
+            _nSteps = value;
+        }
+    }
 }
