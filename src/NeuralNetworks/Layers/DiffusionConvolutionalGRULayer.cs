b2b0a58895d25ca7688f9cb75eb8d075b3efafaf
Franklin Moormann <cheatcountry@gmail.com>
2026-08-26 20:33:22 -0400

    fix: fix TOTO zero-warmup and DCGRU shape validation

--- src/NeuralNetworks/Layers/DiffusionConvolutionalGRULayer.cs
+++ src/NeuralNetworks/Layers/DiffusionConvolutionalGRULayer.cs
@@ -240,7 +240,7 @@ internal sealed class DiffusionConvolutionalGRULayer<T> : LayerBase<T>, IShapeCo
             || input.Shape[2] != _inputSize)
         {
             throw new ArgumentException(
-                $"DCGRU sequence input must be [{_numNodes}, time, {_inputSize}].", nameof(input));
+                $"DCGRU sequence input shape must be [{_numNodes}, time, {_inputSize}] (nodes, time, features).", nameof(input));
         }
     }
 
