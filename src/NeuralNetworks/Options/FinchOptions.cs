bb44595e5174cd2f7bf48689230a25eb271a8ff8
Franklin Moormann <cheatcountry@gmail.com>
2026-09-11 16:07:06 -0400

    fix(options): keep constructor validation assembly-internal

--- src/NeuralNetworks/Options/FinchOptions.cs
+++ src/NeuralNetworks/Options/FinchOptions.cs
@@ -98,7 +98,7 @@ public class FinchOptions : SequenceModelOptions
     /// <exception cref="ArgumentException">
     /// Thrown when a required dimension is zero or negative.
     /// </exception>
-    public void Validate()
+    internal void Validate()
     {
         ValidateCore(requiresHeads: true, requiresState: false);
     }
